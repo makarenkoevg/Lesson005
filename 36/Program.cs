8b42e7bb10ef9b07267b3c2e78169a3f97029534
makarenkoevg <makarenkoevg@mail.ru>
2022-12-13 04:28:05 +0500

    36

---
+++ 36/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях (не индексах, а позициях!!!)
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество элементов массива: ");
+int rows = int.Parse(ReadLine());
+
+int[] array = GetArray(rows, 0, 10);
+PrintArray(array);
+
+WriteLine($"=> {Summ(array)}");
+
+int[] GetArray(int m, int min, int max)
+{
+    int[] result = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
+int Summ(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        sum+=array[i];
+    }
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+      
+            Write($"{array[i]} ");
+
+    }
+
+}
