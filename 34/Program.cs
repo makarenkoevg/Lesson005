aa323f58a7636980bbadff92914b771dcab6ff30
makarenkoevg <makarenkoevg@mail.ru>
2022-12-13 04:13:39 +0500

    34

--- 34/Program.cs
+++ 34/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество элементов массива: ");
+int rows = int.Parse(ReadLine());
+
+int[] array = GetArray(rows, 100, 999);
+PrintArray(array);
+
+WriteLine($"=> {Count(array)}");
+
+int[] GetArray(int m, int min, int max)
+{
+    int[] result = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((array[i] % 2) == 0)
+        {
+            count+=1;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+      
+            Write($"{array[i]} ");
+
+    }
+}
